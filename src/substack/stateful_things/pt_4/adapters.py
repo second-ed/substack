a920519bdee479c1b621944b2f283106f8199943
ed cuss <edcussmusic@gmail.com>
2026-02-04 19:32:51 +0000

    feat: end of stateful things examples

--- src/substack/stateful_things/pt_4/adapters.py
+++ src/substack/stateful_things/pt_4/adapters.py
@@ -1,18 +1,35 @@
 from __future__ import annotations
 
+from collections.abc import Hashable
+from types import MappingProxyType
+
+import attrs
 import polars as pl
+from attrs.validators import deep_mapping, instance_of, is_callable
 
-from .io_funcs import READ_FUNCS, WRITE_FUNCS, ReadFn, WriteFn
+from .io_funcs import READ_FUNCS, WRITE_FUNCS, Data, ReadFn, WriteFn
 
 
+@attrs.define
 class IoBase:
-    def __init__(
-        self,
-        read_fns: dict[str, ReadFn] = READ_FUNCS,
-        write_fns: dict[str, WriteFn] = WRITE_FUNCS,
-    ) -> None:
-        self.read_fns = read_fns
-        self.write_fns = write_fns
+    read_fns: MappingProxyType[Hashable, ReadFn] = attrs.field(
+        default=READ_FUNCS,
+        validator=deep_mapping(
+            key_validator=instance_of(Hashable),
+            value_validator=is_callable(),
+            mapping_validator=instance_of(MappingProxyType),
+        ),
+        converter=MappingProxyType,
+    )
+    write_fns: MappingProxyType[Hashable, WriteFn] = attrs.field(
+        default=WRITE_FUNCS,
+        validator=deep_mapping(
+            key_validator=instance_of(Hashable),
+            value_validator=is_callable(),
+            mapping_validator=instance_of(MappingProxyType),
+        ),
+        converter=MappingProxyType,
+    )
 
     def read(self, path: str, file_type: str) -> dict | pl.DataFrame:
         if file_type not in self.read_fns:
@@ -25,18 +42,19 @@ class IoBase:
         return self.write_fns[file_type](data, path)
 
 
+@attrs.define
 class RealIoV2(IoBase):
     pass
 
 
+@attrs.define
 class FakeIoV2(IoBase):
-    def __init__(
-        self,
-        read_fns: dict[str, ReadFn] = READ_FUNCS,
-        write_fns: dict[str, WriteFn] = WRITE_FUNCS,
-    ) -> None:
-        self.read_fns = dict.fromkeys(read_fns, self._read_fn)
-        self.write_fns = dict.fromkeys(write_fns, self._write_fn)
+    files: dict[str, Data] = attrs.field(factory=dict, validator=instance_of(dict))
+    log: list[dict] = attrs.field(factory=list, validator=instance_of(list))
+
+    def __attrs_post_init__(self) -> None:
+        self.read_fns = MappingProxyType(dict.fromkeys(self.read_fns.keys(), self._read_fn))
+        self.write_fns = MappingProxyType(dict.fromkeys(self.write_fns.keys(), self._write_fn))
 
     def _read_fn(self, path: str, file_type: str) -> dict | pl.DataFrame:
         self.log.append({"func": "read", "path": path, "file_type": file_type})
